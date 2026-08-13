268a10fc5f438a4e6ac0e34802253f06c2e9b1f2
Sami Hasan <149407021+Samihasan747@users.noreply.github.com>
2026-08-14 02:56:59 +0600

    Add Elephant [A] from Codeforces

---
+++ 617/A - Elephant/solution.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int x,count=0;
+    cin>>x;
+ 
+    if(x%5==0)
+    {
+        count=x/5;
+    }
+    else
+    {
+        count=x/5 + 1;
+    }
+ 
+    cout<<count;
+ 
+}
