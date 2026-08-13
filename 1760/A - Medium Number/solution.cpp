15723616a77683f22201de60e3a589b8b9427fd0
Sami Hasan <149407021+Samihasan747@users.noreply.github.com>
2026-08-14 03:01:40 +0600

    Add Medium Number [A] from Codeforces

---
+++ 1760/A - Medium Number/solution.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[3];
+    while(n--)
+    {
+      for(int i=0 ; i<3 ; i++)
+            cin>>arr[i];
+ 
+        sort(arr,arr+3);
+        cout<<arr[1]<<endl;
+    }
+}
