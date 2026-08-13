db2ccd1a727363d4c23a711bc3dccf0aa9b3fb27
Sami Hasan <149407021+Samihasan747@users.noreply.github.com>
2026-08-14 03:04:42 +0600

    Add Soft Drinking [A] from Codeforces

---
+++ 151/A - Soft Drinking/solution.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int n,k,l,c,d,p,nl,np;
+    cin>>n>>k>>l>>c>>d>>p>>nl>>np;
+    int x,y,z;
+    x = k*l/nl;
+    y = c*d;
+    z = p/np;
+ 
+    int result = min(z,min(x,y));
+ 
+    cout<< result/n;
+ 
+}
