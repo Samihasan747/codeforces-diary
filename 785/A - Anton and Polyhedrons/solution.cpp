a9827873c3e898f26b937ff786192d88cc529a58
Sami Hasan <149407021+Samihasan747@users.noreply.github.com>
2026-08-14 02:59:12 +0600

    Add Anton and Polyhedrons [A] from Codeforces

---
+++ 785/A - Anton and Polyhedrons/solution.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int n,count=0;
+    cin>>n;
+    string arr[n];
+ 
+    for(int i=0 ; i<n ; i++)
+        cin>>arr[i];
+ 
+    for(int i=0 ; i<n ; i++)
+        {
+            if(arr[i] == "Tetrahedron")
+                count+=4;
+            if(arr[i] == "Cube")
+                count+=6;
+            if(arr[i] == "Octahedron")
+                count+=8;
+            if(arr[i] == "Dodecahedron")
+                count+=12;
+            if(arr[i] == "Icosahedron")
+                count+=20;
+        }
+    cout<<count;
+ 
+}
