54a33de98080d5e7cdf189ab099aa1bdb1525a54
Sami Hasan <149407021+Samihasan747@users.noreply.github.com>
2026-08-14 03:04:38 +0600

    Add Police Recruits [A] from Codeforces

---
+++ 427/A - Police Recruits/solution.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    int crime = 0;
+    int police =0;
+ 
+    for(int i=0 ; i<n ; i++){
+        cin>>arr[i];
+    }
+ 
+    for(int i=0 ; i<n ; i++){
+            if(arr[i] > 0){
+                police+=arr[i];
+            }
+            else if(arr[i] == -1 && police == 0){
+                crime++;
+            }
+            else if(arr[i] == -1 && police > 0){
+                police--;
+            }
+    }
+ 
+ 
+    cout<<crime<<endl;
+ 
+ 
+ 
+ 
+}
