7127c8778bfd86df1c1f22ca242d66a60fa69040
Sami Hasan <149407021+Samihasan747@users.noreply.github.com>
2026-08-14 02:58:31 +0600

    Add Calculating Function [A] from Codeforces

---
+++ 486/A - Calculating Function/solution.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n,s;
+    cin>>n;
+ 
+    if(n%2 == 0)
+    {
+        s = n/2;
+    }
+    else
+    {
+        s = -1*(n+1)/2;
+    }
+ 
+    cout<<s;
+ 
+}
