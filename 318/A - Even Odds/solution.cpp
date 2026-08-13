954601f49aa6aff3121c6bc2fdb318e1568c5819
Sami Hasan <149407021+Samihasan747@users.noreply.github.com>
2026-08-14 03:04:50 +0600

    Add Even Odds [A] from Codeforces

---
+++ 318/A - Even Odds/solution.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    long long m,n;
+    cin>>m>>n;
+    long long k=ceil(m/2.0);
+ 
+ 
+ 
+    if(n<=k){
+        cout<<(2*n - 1);
+    }
+    else{
+        cout<<2*(n-k);
+    }
+ 
+ 
+}
