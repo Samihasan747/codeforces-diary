6ba2aacfd11cdf1bc6608dff2d8120bcc1ef01c3
Sami Hasan <149407021+Samihasan747@users.noreply.github.com>
2026-08-14 03:02:14 +0600

    Add Creating Words [A] from Codeforces

---
+++ 1985/A - Creating Words/solution.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cin>>n;
+    string s1, s2;
+    while(n--)
+    {
+        cin>>s1>>s2;
+ 
+        char temp = s1[0];
+        s1[0] = s2[0];
+        s2[0] = temp;
+ 
+    cout<<s1<<" "<<s2<<endl;
+    }
+ 
+}
