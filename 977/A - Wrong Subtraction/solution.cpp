ffec90ddc83ac7a1cbeeffac58c60f9574acc4c0
Sami Hasan <149407021+Samihasan747@users.noreply.github.com>
2026-08-14 02:57:10 +0600

    Add Wrong Subtraction [A] from Codeforces

---
+++ 977/A - Wrong Subtraction/solution.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    long long n,k,count=0;
+ 
+    cin>>n>>k;
+ 
+    for(int i=0 ; i < k ; i++)
+    {
+        if(n%10 != 0)
+            {
+                n=n-1;
+            }
+        else
+        {
+            n=n/10;
+ 
+        }
+    }
+ 
+    cout<<n;
+ 
+}
