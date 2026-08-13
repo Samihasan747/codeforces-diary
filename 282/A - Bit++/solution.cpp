e4462d602f6cab58b16961f0443341bb7ce6d833
Sami Hasan <149407021+Samihasan747@users.noreply.github.com>
2026-08-14 02:57:35 +0600

    Add Bit++ [A] from Codeforces

---
+++ 282/A - Bit++/solution.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    string s;
+    int x=0,n;
+ 
+    cin>>n;
+ 
+    for(int i=0 ; i<n ; i++)
+    {
+        cin>>s;
+        if(s == "++X" || s == "X++")
+        {
+            x++;
+        }
+        else
+        {
+            x--;
+        }
+    }
+    cout<<x;
+}
+ 
