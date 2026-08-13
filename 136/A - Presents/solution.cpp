0bcdd9044adae1df945c1c3ce36584e01a4e03cd
Sami Hasan <149407021+Samihasan747@users.noreply.github.com>
2026-08-14 03:03:44 +0600

    Add Presents [A] from Codeforces

---
+++ 136/A - Presents/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+ 
+    for(int i=0 ;i<n ; i++)
+    {
+        cin>>arr[i];
+    }
+ 
+    int ans[n+1];
+   for(int i=0 ; i<n+1 ; i++){
+    for(int j=0 ; j<n ; j++){
+        if(arr[j] == i)
+            cout<<j+1<<" ";
+    }
+   }
+}
