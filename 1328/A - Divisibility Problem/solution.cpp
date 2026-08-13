bae3b6e96f8ddb75c9f29b9a5047c2d9a76e361f
Sami Hasan <149407021+Samihasan747@users.noreply.github.com>
2026-08-14 02:58:50 +0600

    Add Divisibility Problem [A] from Codeforces

---
+++ 1328/A - Divisibility Problem/solution.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    long long n,a,b;
+    cin>>n;
+ 
+    for(int i=0 ; i<n ; i++)
+    {
+        cin>>a>>b;
+        if(a%b == 0)
+            cout<<0<<endl;
+        else
+            cout<<(b-(a%b))<<endl;
+ 
+    }
+}
+ 
