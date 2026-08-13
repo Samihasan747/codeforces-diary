25ec03eb4897e24c8b0cafc81bd9e9101b0974aa
Sami Hasan <149407021+Samihasan747@users.noreply.github.com>
2026-08-14 02:58:40 +0600

    Add Ultra-Fast Mathematician [A] from Codeforces

---
+++ 61/A - Ultra-Fast Mathematician/solution.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    string a,b,c;
+    cin>>a>>b;
+    int n=a.size();
+    for(int i=0 ; i<n ; i++)
+    {
+        if(a[i] == b[i])
+            cout<<0;
+        else
+            cout<<1;
+    }
+}
