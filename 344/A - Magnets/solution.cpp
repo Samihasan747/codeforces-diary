8c533172cc4aaf680c313ad884c5d4bc0585e8bd
Sami Hasan <149407021+Samihasan747@users.noreply.github.com>
2026-08-14 02:58:28 +0600

    Add Magnets [A] from Codeforces

---
+++ 344/A - Magnets/solution.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int n,count=0;
+    cin>>n;
+    int arr[n];
+ 
+    for(int i=0 ; i<n ; i++)
+    {
+        cin>>arr[i];
+    }
+ 
+    for(int i=0 ; i<n ; i++)
+    {
+        if(arr[i] != arr[i+1])
+            count++;
+    }
+ 
+    if(n==1)
+        cout<<1;
+    else
+        cout<<count;
+}
