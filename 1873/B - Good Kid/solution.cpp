38f88afe67cae35d7cf4fec9b1858ad15f27ea53
Sami Hasan <149407021+Samihasan747@users.noreply.github.com>
2026-08-14 02:55:18 +0600

    Add Good Kid [B] from Codeforces

---
+++ 1873/B - Good Kid/solution.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,prod=1;
+        cin>>n;
+        int arr[n];
+ 
+        for(int i=0 ; i<n ; i++)
+            cin>>arr[i];
+ 
+        sort(arr,arr+n);
+ 
+        arr[0]++;
+ 
+        for(int i=0 ; i<n ; i++){
+            prod*=arr[i];
+        }
+ 
+        cout<<prod<<endl;
+ 
+    }
+ 
+}
