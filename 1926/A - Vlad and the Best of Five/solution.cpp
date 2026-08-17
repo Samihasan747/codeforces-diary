0d988b314eb351072e3981c52ff962d51d8641d1
Sami Hasan <149407021+Samihasan747@users.noreply.github.com>
2026-08-18 00:05:32 +0600

    Add Vlad and the Best of Five [A] from Codeforces

---
+++ 1926/A - Vlad and the Best of Five/solution.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+ 
+    string s;
+    cin>>s;
+    int counta=0,countb=0;
+ 
+    for(int i=0 ; i< s.size() ; i++){
+        if(s[i] == 'A'){
+            counta++;
+        }else{
+            countb++;
+        }
+    }
+ 
+    cout<<((counta>countb)?"A":"B")<<endl;
+    }
+}
+ 
