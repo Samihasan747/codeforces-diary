2a4310b9d01b48324f5c457738e12a4b44974c70
Sami Hasan <149407021+Samihasan747@users.noreply.github.com>
2026-08-31 02:14:23 +0600

    Add Trippi Troppi [A] from Codeforces

---
+++ 2094/A - Trippi Troppi/solution.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+ 
+ 
+    string x,y,z;
+    cin>>x>>y>>z;
+    string t;
+    t+=x[0];
+    t+=y[0];
+    t+=z[0];
+ 
+    cout<<t<<endl;
+    }
+}
+ 
